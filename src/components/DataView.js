91f9e2f068afdfcdfa6680ea1b2327d8b0ecc518
Mnarine2619 <mnarine2619@richmondhillhs.org>
2021-02-11 19:04:13 +0000

    FinalStreet-easy2

--- src/components/DataView.js
+++ src/components/DataView.js
@@ -19,6 +19,10 @@ function DataView(props) {
               <h2 className="text-data">{props.renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </div>
+            <div className="text">
+           <h2 className="text-data">{props.renterIncome}</h2>
+           <h3 className="text-label">Annual Income</h3>
+         </div>
           </div>
           <div className="data-view-column right">
             <div className="text">
@@ -28,6 +32,10 @@ function DataView(props) {
               <h2 className="text-data">{props.ownerCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </div>
+            <div className="text ">
+        <h2 className="text-data">{props.ownerIncome}</h2>
+       <h3 className="text-label">Annual Income</h3>
+       </div>
           </div>
         </div>
       </div>
